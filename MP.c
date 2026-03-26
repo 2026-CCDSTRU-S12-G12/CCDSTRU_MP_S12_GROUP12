baf77de62a9cad781b8f7ddf7ce77c78a6dc471e
GildedEternal <gildedtriplea@email.com>
2026-03-26 18:42:53 +0800

    Added Interface, Function Skeletons, NextPlayerMove

--- MP.c
+++ MP.c
@@ -18,7 +18,7 @@ typedef struct {
     Purpose: removing a coordinate in a set
     Return: none (modifies the set in the parameter indirectly)
     Example: A = {(1,2), (3,4)} & num_coord = 2, remove (1,2) in Set A, then:
-             A = {(3,4)} and num_coord = 1
+            A = {(3,4)} and num_coord = 1
 */ 
 void
 removeCoord(Set *A, Coord pos)
@@ -44,7 +44,7 @@ removeCoord(Set *A, Coord pos)
     Purpose: adding a coordinate in a set
     Return: none (modifies the set in the parameter indirectly)
     Example: A = {(1,2), (3,4)} & num_coord = 2, add (5,6) in Set A, then:
-             A = {(1,2), (3,4), (5,6)} and num_coord = 3
+            A = {(1,2), (3,4), (5,6)} and num_coord = 3
 */ 
 void
 addCoord(Set *A, Coord pos)
@@ -58,13 +58,15 @@ addCoord(Set *A, Coord pos)
     }
 }
 
+
+
 /*
     Purpose: checks if a given coordinate exists in a set
     Return: either (a) 1 if found; (b) 0 if not
     Example: A = {(1,2), (3,4), (5,6)}, see if (3,4) exists in the set, then:
-             this function returns 1
-             see if (7,8) exists in the set, then:
-             this function returns 0
+            this function returns 1
+            see if (7,8) exists in the set, then:
+            this function returns 0
 */
 int
 isElement(Set A, Coord pos)
@@ -105,6 +107,41 @@ isOver(Set R, Set B, int start, int val)
     return (F.num_coord == 3 || val >= 20 || (!start && (R.num_coord > 0 && B.num_coord == 0) || (R.num_coord == 0 && B.num_coord > 0)));
 }
 
+/*
+    Purpose: prints the current board state to the terminal
+    Return: none
+    Example: 
+*/ 
+void printBoard(int size, Set* R,Set *B,Set *S,Set *T){
+    int i, j;
+    Coord temp;
+    char cR;
+    char cB;
+    char cS;
+    char cT;
+    for(i=1;i<size+1;i++){
+        for(j=1;j<size+1;j++){
+            temp.x = j;
+            temp.y = i;
+            cR = ' ';
+            cB = ' ';
+            cS = ' ';
+            cT = ' ';
+            if(isElement(*R,temp)) cR = 'R';
+            if(isElement(*B,temp)) cB = 'B';
+            if(isElement(*S,temp)) cS = 'S';
+            if(isElement(*T,temp)) cT = 'T';
+            printf("%c%c%c%c",cS,cR,cB,cT);
+            if(j<size){
+                printf("|");
+            }
+        }
+        if(i<size){
+            printf("\n--------------\n");
+        }
+    }
+    printf("\n");
+}
 
 // MAIN FUNCTIONS 
 void
@@ -119,6 +156,52 @@ Remove(Coord pos, int go, Set *R, Set *B, Set *S, Set *T)
     removeCoord(T, pos);
 }
 
+void Replace(Coord pos, int *go, Set *R, Set *B, Set *S, Set *T, Set *M){
+
+}
+
+void Expand(Coord pos, int *go, Set *R, Set *B, Set *S, Set *T, Set *M){
+
+}
+
+void Update(Coord pos, int *go, int* good, Set *R, Set *B, Set *S, Set *T, Set *M){
+
+}
+
+/*
+    Purpose: Parses the next player's move
+    Return: none
+    Example: 
+*/ 
+void NextPlayerMove(Coord pos, int* over, int* start, int* go, int* good, int* val, Set *R, Set *B, Set *S, Set *T, Set *M){    
+    if(!*over){
+        if(*start){
+            if(*go){
+                addCoord(R,pos);
+                addCoord(S,pos);
+                *good = 1;
+            }else{
+                addCoord(B,pos);
+                addCoord(S,pos);
+                *good = 1;
+            }
+        }else{
+            if( (*go && isElement(*R,pos)) || (!*go && isElement(*B,pos))){
+                Update(pos,go,good,R,B,S,T,M);
+                *good = 1;
+            }
+        }
+    }
+    if(*start && R->num_coord > 0 && B->num_coord > 0){
+        *start = 0;
+    }
+    //Im not sure if the order the equations appear in matter or not so I'll just give this its own !over if statement
+    if(!*over && *good){
+        *good = !*good;
+        *go = !*go;
+        *val++;
+    }
+}
 
 
 int main(){
@@ -129,11 +212,13 @@ int main(){
     int C[3] = {1,2,3};
     //Set M
     int size = sizeof(C)/sizeof(int);
-    int M[size][size][2];
+    Set M = {0};
+    Coord temp;
     for(i=1;i<size+1;i++){
         for(j=1;j<size+1;j++){
-            M[i][j][0] = i;
-            M[i][j][1] = j;
+            temp.x = j;
+            temp.y = i;
+            addCoord(&M,temp);
         }
     }
     //Set N
@@ -143,8 +228,6 @@ int main(){
     //V[1] = True/1
     int V[2] = {0,1};
 
-
-
     //System Initialization
     //System Variables
     int good = V[0];
@@ -152,12 +235,56 @@ int main(){
     int start = V[1];
     int found = V[0];
     int val = 0;
+
+    //Student Initialized
+    int over = 0;
     
     Set R = {0};
     Set B = {0};
     Set S = {0};
     Set T = {0};
     Set F;
+
+    //Menu Variables
+    int Active = 1;
+    int MenuScreen = 1;
+    int InGame = 0;
+    char Input = ' ';
+    int xInput = 0, yInput = 0;
+    Coord cInput;
+    while(Active){
+        while(MenuScreen){
+            printf("Welcome to the Game! {Title Screen in Progress}\n");
+            printf("[S] - Start a Match\n");
+            printf("[X] - Exit the Program\n");
+            printf("Input: ");
+            scanf(" %c",&Input);
+            if(Input == 'S'){
+                MenuScreen = 0;
+                InGame = 1;
+            }else if(Input == 'X'){
+                MenuScreen = 0;
+                Active = 0;
+            }
+        }
+        //Temporary UI
+        while(InGame){
+            printf("Board State:\n");
+            printBoard(size,&R,&B,&S,&T);
+            printf("Input x: ");
+            scanf("%d",&xInput);
+            printf("Input y: ");
+            scanf("%d",&yInput);
+            cInput.x = xInput;
+            cInput.y = yInput;
+            // printf("%d %d\n",xInput,yInput);
+            // printf("%d %d %d %d\n",xInput>0 , xInput<size+1 , yInput>0 , yInput<size+1);
+            // printf("%d %d %d\n",R.num_coord,B.num_coord,S.num_coord);
+            if(xInput>0 && xInput<size+1 && yInput>0 && yInput<size+1){
+                NextPlayerMove(cInput,&over,&start,&go,&good,&val,&R,&B,&S,&T,&M);
+            }
+        }
+    }
 
     return 0;
 }
